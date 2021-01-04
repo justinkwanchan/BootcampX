3da194ad5a763273db8a5a8b7248cadfcfc93bac
justinkwanchan <justinkwanchan@gmail.com>
2021-01-04 22:59:05 +0000

    Complete query 2 assignment

---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,5 @@
+SELECT SUM(assignment_submissions.duration) as total_duration
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+JOIN assignment_submissions ON students.id = student_id
+WHERE cohorts.name = 'FEB12';
