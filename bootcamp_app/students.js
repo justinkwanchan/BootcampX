d7c6cd922d1dc1375bb33d209aab0427bd138d91
justinkwanchan <justinkwanchan@gmail.com>
2021-01-07 02:05:43 +0000

    Add sql query to code and take in command-line input

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,3 +6,17 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+
+pool.query(`
+SELECT students.id, students.name as student, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
